530d68feebac85955ab9044fcda96e8f72e4f748
danielcsmg <daniel.csouzamg@gmail.com>
2022-04-01 16:01:30 -0300

    Exercício Três atualizado 2

--- src/exercicioTresVetores.java
+++ src/exercicioTresVetores.java
@@ -3,19 +3,22 @@ import java.util.Scanner;
 public class exercicioTresVetores {
     public static void main(String[] args) {
         Scanner leitor = new Scanner(System.in);
-        String listaAlunos[] = new String[10];
+        int vetor[] = new int[10];
 
-        System.out.println("Algoritmo lista de alunos");
-        System.out.println("Digite o nome de dez alunos:");
-        for (int i = 0; i < 10; i++) {
-            listaAlunos[i] = leitor.nextLine();
+        System.out.println("Algoritmo números pares");
+        System.out.println("Digite dez valores:");
+        for(int i = 0; i < vetor.length; i++){
+            vetor[i] = leitor.nextInt();
         }
 
-        System.out.println("\n\n\n\nLista de alunos");
-        for (int i = 0; i < 10; i++) {
-            System.out.println(listaAlunos[i]);
+        System.out.println("\n\n\n\nLista de números pares:");
+        for(int i = 0; i < vetor.length; i++){
+            if(vetor[i] % 2 == 0){
+                System.out.println(vetor[i]);
+            }
         }
 
         leitor.close();
     }
 }
+

78029d43560286368121a3984e15663f6a50740e
danielcsmg <daniel.csouzamg@gmail.com>
2022-04-01 13:43:24 -0300

    Atualização do exercício Um

--- src/exercicioUmVetores.java
+++ src/exercicioUmVetores.java
@@ -2,14 +2,14 @@ public class exercicioUmVetores {
     public static void main(String[] args) {
         int[] vetorSoma = new int[5];
         vetorSoma[0] = 21;
-        vetorSoma[1] = 21;
+        vetorSoma[1] = 215;
         vetorSoma[2] = 21;
-        vetorSoma[3] = 21;
+        vetorSoma[3] = 0;
         vetorSoma[4] = 21;
         int somaNumeros = 0;
 
-        for(int i = 0; i < vetorSoma.length; i++){
-            somaNumeros += vetorSoma[i];
+        for(int soma : vetorSoma){
+            somaNumeros += soma;
         }
 
         System.out.println("A soma dos números é " + somaNumeros);
